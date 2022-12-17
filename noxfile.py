6e084ce9f80fb3cd96f64eeccc776a7c058a5236
Laszlo Sragner <sragner@gmail.com>
2022-12-17 18:04:16 +0000

    flake8 and black

--- noxfile.py
+++ noxfile.py
@@ -4,4 +4,21 @@ import nox
 @nox.session(python=["3.8"])
 def tests(session):
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest")
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+locations = "nb_query", "tests", "noxfile.py"
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
